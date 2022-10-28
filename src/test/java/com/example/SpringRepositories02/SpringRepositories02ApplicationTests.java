8c3f75a7711b1f7b5981a25249e97cee40e83648
Mario Cotumaccio <mariocotumaccio1@gmail.com>
2022-10-28 05:57:58 +0200

    Creating DB and server port on application.yml

---
+++ src/test/java/com/example/SpringRepositories02/SpringRepositories02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringRepositories02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRepositories02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
